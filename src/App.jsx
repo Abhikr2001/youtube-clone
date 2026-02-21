804416a3130d4f01f274f41fd37c6e66bd6b828f
Abhikr2001 <123152383+Abhikr2001@users.noreply.github.com>
2026-02-21 10:11:30 +0530

    Fix import paths for Home and Video components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Video from './pages/Video/Video'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 import Footer from './Components/Footer/Footer'
 
 const App = () => {
